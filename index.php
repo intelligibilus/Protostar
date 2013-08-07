d1d7ae760bcab4f3fba9d3cc58d4d8f80a6fcb9d
Bob Bloom <info@lasallemart.com>
2013-08-07 15:22:59 -0400

    Correction: add <? ?> to new IF statement in index.php

--- index.php
+++ index.php
@@ -203,10 +203,12 @@ else
 			<hr />
 			<jdoc:include type="modules" name="footer" style="none" />
 
+            <?php
             if ($this->params->get('backtotop'))
             {
          	    "<p class=\"pull-right\"><a href=\"#top\" id=\"back-top\"><?php echo JText::_('TPL_PROTOSTAR_BACKTOTOP'); ?></a></p>";
             }
+            ?>
 			<p>&copy; <?php echo $sitename; ?> <?php echo date('Y');?></p>
 		</div>
 	</footer>
